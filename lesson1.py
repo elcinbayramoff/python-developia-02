2d7f937ac668bfe3b48ad32333c96fe392122929
Elchin <bayramlielcin03@gmail.com>
2025-03-12 06:24:27 +0400

    Lesson2

---
+++ lesson1.py
@@ -0,0 +1,121 @@
+#Bura commentdir
+"""asdasda
+asda
+asdas
+
+
+asd
+
+ad
+ads
+
+asd
+
+sadasd"""
+
+"""dfskfbdsjkf"""
+
+"""
+print('Salam','Necesen')
+print('Yaxsiyam')"
+"""
+
+# print("Python")
+# print("    ən populyar")
+# print("        dildir")  
+
+# print('---------------------')
+# print('|                    |')
+# print('|                    |')
+# print('|                    |')
+# print('---------------------')'
+
+
+# a = 200
+# a = 100
+# print(a)
+
+# a = input("a-ni daxil edin: ")
+
+# a = int(input("a-ni daxil edin: "))
+
+# # b = input("b-ni daxil edin: ")
+# b = int(input("b-ni daxil edin: "))
+
+# print('a-nın dəyəri:', a)
+# print('b-nın dəyəri:', b)
+
+# c = a + b
+# "10 + 20 = 30"
+# print(a,'+',b,'=',c)
+
+#int()
+
+# a = "100"
+# print(type(a))
+# b = int(a)
+# print(type(b))
+
+# a = 6
+# b = 4
+
+# # print(a / b)
+
+# print(a // b)
+# print(a % b)
+
+# a = 5
+# a *= 2 # a = a * 2
+# print(a)
+# a = 5
+# a **= 5
+# print(a)
+# a = 1234.567
+
+# formatted_a = "{:20.1f}".format(a)
+
+# print(formatted_a)
+
+# a = -5
+# print(abs(a))
+
+# print(int('Salam'))
+
+# a = 1234.567
+# rounded_a = round(a, 1)
+# print(type(rounded_a))
+
+# print(min(4,1,4,5,6))
+# print(max(4,1,4,5,6))
+
+# 4 ** 3
+# pow(4, 3)
+
+# import math
+
+# a = math.floor(4.5)
+# b = math.ceil(4.5)
+# print(a)
+
+# a = 4.5
+# int_a = int(a)
+# print(int_a)
+
+# a = 5.5
+# print(round(a))
+
+# print(math.pi)
+
+#Tapsiriq 1
+a = int(input("a-ni daxil edin: "))
+
+# b = input("b-ni daxil edin: ")
+b = int(input("b-ni daxil edin: "))
+
+c = int(input("c-ni daxil edin: "))
+
+
+d = a + b + c
+e = a * b * c
+f = d / 3
+print(d, e, f)
