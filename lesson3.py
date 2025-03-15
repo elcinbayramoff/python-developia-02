faa1736368993cde939fe963c1f54fb36149c3fe
Elchin <bayramlielcin03@gmail.com>
2025-03-15 08:12:28 +0400

    lesson3

---
+++ lesson3.py
@@ -0,0 +1,131 @@
+#Klaviaturadan üç tam ədədi daxil edin, onların cəmini, hasilini və ədədi ortasını tapın
+
+# a = int(input('1-ci ədədi daxil edin: '))
+# b = int(input('2-ci ədədi daxil edin: '))
+# c = int(input('3-cü ədədi daxil edin: '))
+
+# cem = a + b + c
+# hasil = a * b * c
+# ededi_orta = cem / 3
+
+# print('Cem:', cem, 'Hasil:', hasil, 'Ededi orta:', ededi_orta)
+
+#Task 2
+
+# import math
+
+# x1 = float(input('x1-i daxil edin: '))
+# x2 = float(input('x2-i daxil edin: '))
+
+# y1 = float(input('y1-i daxil edin: '))
+# y2 = float(input('y2-i daxil edin: '))
+
+# length = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+# print('Length',length)
+
+
+#Task 3
+
+# a = input('3reqemli eded daxil edin: ')
+
+# digit1 = a % 10 # 3
+# a = a // 10
+# digit2 = a % 10 # 12 % 10 = 2
+# a = a // 10 
+# digit3 = a % 10
+# print(digit1, digit2, digit3)
+
+# Stringler
+
+# a = "I don't like Harry Potter"
+# print("I don't like Harry Potter")
+
+# a = """
+# Salam
+# Necesen?
+# """
+# print(a)
+#   -654321
+# a = 'Python'
+#    012345
+
+# print(a[6])
+
+# print( a[-5:-1])
+# b = 'sdjfnkjdsfbnkjbdsfkjbdskjfbskjdbfjksbdfjkbdskjbfkjsdbO'
+# print(b[-1])#Sonuncu simvol
+
+
+# a = 'Python'
+
+#1 
+# print(a[1:3]) # yt
+
+#2 
+# print(a[:3]) # Pyt
+
+#3 
+# print(a[1:]) # ython
+
+# a = 'Salam, necəsən?'
+#malaS
+# print( a[2:-4:2]   )
+# print( a[4:-20:-1]   )
+
+# a = 'Salam'
+# b = 'Necesen'
+# print(a + b)
+
+# a = 'Python' # Pyshon
+
+# part1 = a[:2]
+# part2 = a[3:]
+# result = part1 + 's' + part2
+
+# print(result)
+
+#Python's age is 33
+
+# a = 33
+# b = "Python's age is" + str(a)
+# print("Python's age is",a)
+# gun = 30
+# ay = 10
+# il = 2000
+
+# menim doguldugum gun 30/10/2000-dir
+# age = 33
+# day =100
+# a = f"Python's age is {age}"
+# print(a)
+
+# gun = 30
+# ay = 10                     
+# il = 2000
+
+# result = f"menim doguldugum \t gun {gun}/{ay}/{il}-dir"
+# print(result)
+
+# a = 'hello world'
+# b = a.capitalize()
+
+# print(b)
+# b = a.find('Dunya')
+# b = a.index('Dunya')
+
+# print(b)
+# a = 'Python'
+# b = a.replace('t','s')
+# print(b)
+# print(len(a))
+
+# a = '..._Python_...'
+# print(a.strip('._'))
+
+# a = 'hello_world'
+
+# print(a.split('_'))
+
+# a = ['a', 'b', 'c']
+# print(','.join(a))
+
