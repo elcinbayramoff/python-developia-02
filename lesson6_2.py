b6ed749a49efdf79280a218b9041c7a70e48cb2c
Elchin <bayramlielcin03@gmail.com>
2025-04-02 06:52:23 +0400

    lesson7

--- lesson6_2.py
+++ lesson6_2.py
@@ -25,13 +25,102 @@
 
 # print(A)
 
-_sum = 0
-# for i in range(1,10,2):
-#     _sum += i
+# _sum = 0
+# # for i in range(1,10,2):
+# #     _sum += i
 
 
-for i in range(1,10):
-    if i % 2 == 1:
-        _sum += i
+# for i in range(1,10):
+#     if i % 2 == 1:
+#         _sum += i
 
-print(_sum)
+# print(_sum)
+
+#Task1
+# _sum = 0
+
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         # _sum = _sum + i
+#         print(i)
+#         _sum += i
+
+# print(f'Sum: {_sum}')
+
+#Task 3
+
+# count = 0
+
+# for i in range(1, 51):
+#     if i % 3 == 0:
+#         print(i)
+#         count += 1
+
+# print(f'Count: {count}')
+
+#Task 4
+"""
+7x1=7
+7x2=14
+...
+7x10=70
+"""
+
+# for i in range(1, 11):
+#     print(f'7x{i}={7 * i}')
+
+#Task 5
+
+# A = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# # 4 3 2 1 0
+# B = []
+
+# for i in range(len(A)-1, -1, -1):
+#     B.append(A[i])
+
+# print(A[::-1])
+
+#Task 2
+
+#0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ... 
+# a = 0
+# b = 1
+
+# _sum = 0
+# for i in range(10):
+#     _sum += a
+#     a, b = b, a + b
+
+# print(_sum)
+# _count = 0
+# for i in range(-2, 2):
+#     _count += 1
+
+# print(_count)
+
+# for i in range(10):
+
+#     if i == 5:
+#         ...
+
+#     print(i)
+#7x1=7
+# print('--------------------------')
+# for a in range(1, 10):
+#     for b in range(1, 10):
+#         print(f'{a}x{b}={a*b}')
+#     print('--------------------------')
+
+# A = [[1,2,3],[4,5,6],[7,8,9]]
+
+# for i in A:
+#     # print(i)
+#     for j in i:
+#         print(j)
+
+    
+
+
+
+
+
---
+++ lesson7.py
@@ -0,0 +1,25 @@
+#while dovru
+
+# count = 5
+# while count > 0:
+#     if count != 2:
+#         4
+#         break
+
+# print(count)
+
+# a = int(input('Ededi daxil edin:'))
+
+# while a < 0:
+#     print('Menfi eded daxil etdiniz')
+#     a = int(input('Ededi daxil edin:'))
+
+# print('Tebrikler, dogru daxil etdiniz!')
+
+# while True:
+#     a = int(input('Ededi daxil edin:'))
+#     if a >= 0:
+#         print('Tebrikler dogru eded daxil etdiniz')
+#         break
+#     print('Menfi daxil etdiniz!')
+
