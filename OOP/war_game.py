03d42be2468b9d875840d45367cffb05211bb7e8
Elchin <bayramlielcin03@gmail.com>
2025-06-11 07:46:50 +0400

    lesson25

--- OOP/war_game.py
+++ OOP/war_game.py
@@ -44,12 +44,11 @@ class Character:
         if not self.is_alive:
             return
         
-        A = [1,1,1,1,2]
+        A = [1,1,1,1,2] # 20%
         damage = self.attack_power * random.choice(A)
         #Hero critically damaged Enemy
         #Hero damaged Enemy
         target.take_damage(damage)
-
 
 class Hero(Character):
     def __init__(self, name, hp, attack, defense):
@@ -57,15 +56,20 @@ class Hero(Character):
         self.inventory = []
         self.equipped_weapon = None
 
-    def equip(self, weapon):
+    def equip(self, weapon: "Weapon"):
         self.equipped_weapon = weapon
-        self.attack_power = ... # Bura Weapon yaradanda qayidacayiq
+        self.attack_power = weapon.damage
 
     def show_status(self):
-        weapon_name = ... # Bura Weapon yaradanda qayidacayiq
+        # if self.equipped_weapon:
+        #     weapon_name = self.equipped_weapon.name
+        # else:
+        #     weapon_name = 'None'
         
+        # weapon_name = self.equipped_weapon.name if self.equipped_weapon else 'None'
+
         print(f'{self.name}')
-        print(f'HP: {self.hp}/{self.max_hp}')
+        print(f'HP: {self.hp}/{self.max_hp}') # 70/100
         print(f'Attack: {self.attack_power} Defense: {self.defense}')
         print(f'Weapon: {weapon_name}')
         print('Inventory:')
@@ -126,10 +130,24 @@ class Enemy(Character):
         return f'{self.kind} (HP: {self.hp}/{self.max_hp})'
 
 class Item:
-    ...
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def use(self, hero: Hero):
+        print(f'{self.name} does nothing')
+    
+    def __str__(self):
+        return f"{self.name} - {self.description}"
+
 
 class Weapon(Item):
-    ...
+    def __init__(self, name, damage):
+        super().__init__(name, f'Gives {damage} damage')
+        self.damage = damage
+    
+    def use(self, hero: Hero):
+        hero.equip(self)
 
 class Potion(Item):
     ...
@@ -137,7 +155,20 @@ class Potion(Item):
 class Room:
     ...
 
-class Game
+class Game:
+    ...
 
 
 
+"""
+a
+əgər a cüt ədəddirsə result = 'Cut', tək ədəddirsə, result = 'Tek' a % 2 == 0
+result
+"""
+
+# if self.equipped_weapon:
+#     weapon_name = self.equipped_weapon.name
+# else:
+#     weapon_name = 'None'
+
+# weapon_name = self.equipped_weapon.name if self.equipped_weapon else 'None'
